98b75eeade8a71406f5252f1fbf31e6d303f1afa
JohanABlomqvist <Therionpoe@gmail.com>
2023-04-05 12:32:15 +0000

    removed save function as option, made it automatically add name and fortune to sheet.

--- run.py
+++ run.py
@@ -28,10 +28,6 @@ def print_fortune_cookie():
     print("  ┃                             ┃")
     print("  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛")
 
-def save_fortune(name, fortune):
-    with open("saved_fortunes.txt", "a") as f:
-        f.write(f"{name}: {fortune}\n")
-
 def get_valid_input(prompt, valid_choices):
     while True:
         user_input = input(prompt).lower()
@@ -40,21 +36,30 @@ def get_valid_input(prompt, valid_choices):
         else:
             print("Invalid input. Please try again.")
 
+def get_valid_name(prompt):
+    while True:
+        name = input(prompt)
+        if name.isalpha():
+            return name
+        else:
+            print("Please enter a valid name containing only letters.")
+
+def save_fortune_in_sheet(fortune):
+    for i in range(1, 301):
+        cell_value = worksheet.cell(i, 10).value
+        if not cell_value:
+            worksheet.update_cell(i, 10, fortune)
+            break
+
 print_fortune_cookie()
 print("Hello, please enter your name to get your fortune:")
-name = input()
+name = get_valid_name("Name: ")
 
 while True:
     fortune = get_fortune()
     print(f"\n{name}, your fortune is:\n")
     print(fortune)
-
-    save_choice = get_valid_input("Would you like to save your fortune? (y/n): ", ["y", "n"])
-    if save_choice == "y":
-        save_fortune(name, fortune)
-        print("Your fortune has been saved.")
-    else:
-        print("Your fortune has not been saved.")
+    save_fortune_in_sheet(fortune)
 
     another_fortune = get_valid_input("Do you want to get another fortune? (y/n): ", ["y", "n"])
     if another_fortune != "y":
