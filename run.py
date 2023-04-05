60abf939e9637beb218ea4cd4706655fa52fab1c
JohanABlomqvist <Therionpoe@gmail.com>
2023-04-05 13:17:07 +0000

    added so that it saves name and fortune to sheet.

--- run.py
+++ run.py
@@ -44,11 +44,11 @@ def get_valid_name(prompt):
         else:
             print("Please enter a valid name containing only letters.")
 
-def save_fortune_in_sheet(fortune):
+def save_name_and_fortune_in_sheet(name, fortune):
     for i in range(1, 301):
         cell_value = worksheet.cell(i, 10).value
         if not cell_value:
-            worksheet.update_cell(i, 10, fortune)
+            worksheet.update_cell(i, 10, f"{name}: {fortune}")
             break
 
 print_fortune_cookie()
@@ -59,7 +59,7 @@ while True:
     fortune = get_fortune()
     print(f"\n{name}, your fortune is:\n")
     print(fortune)
-    save_fortune_in_sheet(fortune)
+    save_name_and_fortune_in_sheet(name, fortune)
 
     another_fortune = get_valid_input("Do you want to get another fortune? (y/n): ", ["y", "n"])
     if another_fortune != "y":
